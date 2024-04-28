ea7c35cec69727abaaab16f2887306250e5a98af
Catois <quentincatois@gmail.com>
2024-04-28 14:37:36 +0200

    customizing interface Admin models (show data in column, filtering option)

--- mywebsite/app/admin.py
+++ mywebsite/app/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(UserAccount)
+
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email', 'is_staff', 'is_active')
+    search_fields = ('first_name', 'last_name', 'email')
+
+class ticketsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ville', 'lieu', 'date', 'heure', 'tickets_prix', 'nombre_total_tickets', 'description', 'slug')
+    search_fields = ('name', 'ville', 'lieu', 'date', 'heure', 'tickets_prix', 'nombre_total_tickets', 'description', 'slug')
+
+admin.site.register(UserAccount, UserAccountAdmin)
 admin.site.register(categories_offres)
-admin.site.register(tickets)
+admin.site.register(tickets, ticketsAdmin)
